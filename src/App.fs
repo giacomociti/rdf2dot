ff0405a3627e656da07c1512f6e5b86598d076c9
citig <giacomo.citi@ynap.com>
2022-05-21 16:28:57 +0200

    shorten named nodes with prefixes

--- src/App.fs
+++ src/App.fs
@@ -5,40 +5,47 @@ open Fable.Core
 open Fable.Core.JsInterop
 open Graph
 
-let getSubject (x: obj) =
-    match x?termType with
-    | "NamedNode" -> Subject.Uri x?value
-    | "BlankNode" -> Subject.Blank x?value
-    | _ -> failwith $"unknown subject {x}"
-
-let getPredicate (x: obj) =
-    match x?termType with
-    | "NamedNode" -> 
-        if x?value = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
-        then Predicate.RdfType
-        else Predicate.Uri x?value
-    | _ -> failwith $"unknown predicate {x}"    
-
-let getObject (x: obj) =
-    match x?termType with
-    | "NamedNode" -> Object.Uri x?value
-    | "BlankNode" -> Object.Blank x?value
-    | "Literal" -> Object.Literal x?value
-    | _ -> failwith $"unknown object {x}"    
-
-let triple (quad: obj) =
-    getSubject quad?subject, getPredicate quad?predicate, getObject quad?object
-
-let getDot quads =
-    quads
-    |> Array.map triple 
-    |> Array.toList
-    |> Graph.fromTriples
-    |> Dot.fromGraph
-
-
 [<ImportAll("./Renderer.js")>]
 let renderer: obj = jsNative    
 
 let render(rdf, selection) =
     renderer?render(rdf, selection)
+
+let shrinkWith (prefixMap: obj) (x: obj) =
+    let result = prefixMap?shrink(x)
+    if isNull result
+    then x?value
+    else result?value
+
+let getSubject shrink (x: obj) =
+    match x?termType with
+    | "NamedNode" -> Subject.Uri (shrink x)
+    | "BlankNode" -> Subject.Blank x?value
+    | _ -> failwith $"unknown subject {x}"
+
+let getPredicate shrink (x: obj) =
+    match x?termType with
+    | "NamedNode" -> 
+        if x?value = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+        then Predicate.RdfType
+        else Predicate.Uri (shrink x)
+    | _ -> failwith $"unknown predicate {x}"    
+
+let getObject shrink (x: obj) =
+    match x?termType with
+    | "NamedNode" -> Object.Uri (shrink x)
+    | "BlankNode" -> Object.Blank x?value
+    | "Literal" -> Object.Literal x?value
+    | _ -> failwith $"unknown object {x}"    
+
+let triple shrink (quad: obj) =
+    getSubject shrink quad?subject, 
+    getPredicate shrink quad?predicate, 
+    getObject shrink quad?object
+
+let getDot (quads, prefixMap: obj) =
+    quads
+    |> Array.map (triple (shrinkWith prefixMap))
+    |> Array.toList
+    |> Graph.fromTriples
+    |> Dot.fromGraph
--- src/Renderer.js
+++ src/Renderer.js
@@ -1,20 +1,22 @@
 import { Parser } from "n3";
 import * as d3 from "d3-graphviz";
 import { getDot } from "./App.fs.js";
+import rdf from '@rdfjs/data-model';
+import PrefixMap from '@rdfjs/prefix-map';
 
-export function render(rdf, selection) {
-    var quads = new Array();
-    new Parser().parse(rdf, (error, quad, prefixes) => {
+export function render(rdfInput, selection) {
+    const quads = new Array();
+    new Parser().parse(rdfInput, (error, quad, prefixes) => {
         if (error){
             console.log(error);
         }
         else if (quad){
-            console.log(quad);
             quads.push(quad);
         }
         else if (prefixes){
-            console.log(prefixes);
-            var dot = getDot(quads);
+            const entries = Object.entries(prefixes).map((x) => [x[0], rdf.namedNode(x[1])])
+            const prefixMap = new PrefixMap(entries, { factory: rdf })
+            var dot = getDot(quads, prefixMap);
             d3.graphviz(selection).renderDot(dot)
         }
     })
