1c3c9ef5492174aaedc17dc9079820dbb9fd3e18
Giacomo Citi <giacomo.citi@zazuko.com>
2023-09-07 17:52:31 +0200

    escape quotes in dot

--- docs/code/toDot.js
+++ docs/code/toDot.js
@@ -1,43 +1,51 @@
 const toDot = DataFactory => dataset => {
   const { namedNode, literal, defaultGraph, quad } = DataFactory
   const rdfType = namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
+  const node = namedNode('http://view/Node')
+  const link = namedNode('http://view/Link')
+  const source = namedNode('http://view/source')
+  const target = namedNode('http://view/target')
   const label = namedNode('http://view/label')
   const color = namedNode('http://view/color')
   const shape = namedNode('http://view/shape')
+  const style = namedNode('http://view/style')
+  const arrowhead = namedNode('http://view/arrowhead')
   const tooltip = namedNode('http://view/tooltip')
-  const source = namedNode('http://view/source')
-  const target = namedNode('http://view/target')
-  const node = namedNode('http://view/Node')
-  const link = namedNode('http://view/Link')
-  
-  const attributes = new Map([
-      [label.value, "label"],
-      [color.value, "color"],
-      [shape.value, "shape"],
-      [tooltip.value, "tooltip"]
-  ])
+
 
   const escape = text => text.replaceAll('"','\\"')
+  const isHTML = x => x.startsWith('<') && x.endsWith('>')
+  const formatLabel = x => isHTML(x)? x : `"${escape(x)}"`
+
+  const attributes = new Map([
+      [label.value, x => `label=${formatLabel(x)}`],
+      [color.value, x => `color=${x}`],
+      [shape.value, x => `shape=${x}`],
+      [style.value, x => `style=${x}`],
+      [arrowhead.value, x => `arrowhead=${x}`],
+      [tooltip.value, x => `tooltip="${escape(x)}"`]
+  ])
+
+  const getAttributes = subject => {
+    const result = []
+    for (const quad of dataset.match(subject)) {
+      const attr = attributes.get(quad.predicate.value)
+      if (attr) {
+          result.push(attr(quad.object.value))
+      }
+    }
+    return result
+  }
   
   const nodes = Array.from(dataset.match(null, rdfType, node)).map(x => {
-      const attrs = []
-      for (const quad of dataset.match(x.subject)) {
-          if (attributes.has(quad.predicate.value)) {
-              attrs.push(`${attributes.get(quad.predicate.value)}="${escape(quad.object.value)}"`)
-          }
-      }
+      const attrs = getAttributes(x.subject)
       return `"${escape(x.subject.value)}" [${attrs.join(',')}]`
   })
   
   const links = Array.from(dataset.match(null, rdfType, link)).map(x => {
       const [src] = dataset.match(x.subject, source)
       const [tgt] = dataset.match(x.subject, target)
-      const attrs = []
-      for (const quad of dataset.match(x.subject)) {
-          if (attributes.has(quad.predicate.value)) {
-              attrs.push(`${attributes.get(quad.predicate.value)}="${escape(quad.object.value)}"`)
-          }
-      }
+      const attrs = getAttributes(x.subject)
       return `"${escape(src.object.value)}" -> "${escape(tgt.object.value)}" [${attrs.join(',')}]`
   })
       
