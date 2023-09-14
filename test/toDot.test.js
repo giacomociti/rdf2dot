90a535a60770c179e65636551a3fa6623854fbe6
Giacomo Citi <giacomo.citi@zazuko.com>
2023-09-14 17:09:39 +0200

    move parsing

--- test/toDot.test.js
+++ test/toDot.test.js
@@ -4,7 +4,6 @@ import { fileURLToPath } from 'url'
 import fs from 'fs'
 import approvals from 'approvals'
 import toDot from '../lib/toDot'
-import { Parser, Store } from 'n3'
 
 const dirname = path.join(path.dirname(fileURLToPath(import.meta.url)), 'support')
 const support = name => fs.readFileSync(path.join(dirname, name)).toString()
@@ -12,7 +11,8 @@ const support = name => fs.readFileSync(path.join(dirname, name)).toString()
 test('to dot', () => {
     const testName = 'test01'
     const diagramText = support(`${testName}.ttl`)
-    const quads = new Parser().parse(diagramText)
-    const dot = toDot(new Store(quads))
+
+    const dot = toDot(diagramText)
+    
     approvals.verify(dirname, testName, dot)
 })
