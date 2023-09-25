f770f3d35f9332660a702bde9d4c00be93cf8d68
Giacomo Citi <giacomo.citi@zazuko.com>
2023-09-25 16:26:55 +0200

    schema visualization

--- doc/examples.js
+++ doc/examples.js
@@ -3,6 +3,8 @@ import createSVG from "../lib/createSVG"
 import rulesBasic from '../rules/basic.n3?raw'
 import rulesDefault from '../rules/default.n3?raw'
 import data1 from './data2.ttl?raw'
+import schema from '../examples/schema.n3?raw'
+import vocabulary1 from '../public/vocabulary.ttl?raw'
 import barnard59 from '../examples/barnard59/barnard59.n3?raw'
 import pipeline1 from '../examples/barnard59/pipeline1.ttl?raw'
 
@@ -15,4 +17,5 @@ const showGraph = async (data, rules, graph) => {
 document.getElementById('data1').textContent = data1
 showGraph(data1, rulesBasic, 'basic1')
 showGraph(data1, rulesDefault, 'default1')
+showGraph(vocabulary1, schema, 'vocabulary1')
 showGraph(pipeline1, barnard59, 'pipeline1')
