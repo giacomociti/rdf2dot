bef434b2b94d18e5073063a162830a90c2699a9b
Giacomo Citi <giacomo.citi@zazuko.com>
2023-09-25 11:19:48 +0200

    add pipeline example

--- doc/examples.js
+++ doc/examples.js
@@ -3,6 +3,8 @@ import createSVG from "../lib/createSVG"
 import rulesBasic from '../rules/basic.n3?raw'
 import rulesDefault from '../rules/default.n3?raw'
 import data1 from './data2.ttl?raw'
+import barnard59 from '../examples/barnard59/barnard59.n3?raw'
+import pipeline1 from '../examples/barnard59/pipeline1.ttl?raw'
 
 const showGraph = async (data, rules, graph) => {
     const diagramText = await applyRules(data, rules)
@@ -13,3 +15,4 @@ const showGraph = async (data, rules, graph) => {
 document.getElementById('data1').textContent = data1
 showGraph(data1, rulesBasic, 'basic1')
 showGraph(data1, rulesDefault, 'default1')
+showGraph(pipeline1, barnard59, 'pipeline1')
