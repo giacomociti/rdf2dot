658ab38d55edfd4989b0793fc09528dc31141d34
Giacomo Citi <giacomo.citi@zazuko.com>
2023-10-14 09:13:21 +0200

    test default rules

--- test/rules.test.js
+++ test/rules.test.js
@@ -10,23 +10,12 @@ const { namedNode } = DataFactory
 const dirname = path.dirname(fileURLToPath(import.meta.url))
 const support = name => fs.readFileSync(path.join(dirname, 'support', name)).toString()
 
-const basicRules = fs.readFileSync(path.resolve(dirname, '..', 'rules', 'basic.n3'))
-
 const isSuccess = result => {
     const dataset = new Store(new Parser().parse(result))
     const rdfType = namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
     const earlPass = namedNode('http://www.w3.org/ns/earl#Pass')
     return dataset.match(null, rdfType, earlPass).size === 1
 }
-
-test('basic rules', async () => {
-    const testName = 'basic01'
-    const test = support(`${testName}.n3`)
-
-    const result = await applyRules(test, basicRules)
-   
-    expect(isSuccess(result)).toEqual(true)
-})
 
 test('custom rules', async () => {
     const testName = 'custom01'
@@ -36,3 +25,23 @@ test('custom rules', async () => {
    
     expect(isSuccess(result)).toEqual(true)
 })
+
+const basicRules = fs.readFileSync(path.resolve(dirname, '..', 'rules', 'basic.n3'))
+test('basic rules', async () => {
+    const testName = 'basic01'
+    const test = support(`${testName}.n3`)
+
+    const result = await applyRules(test, basicRules)
+   
+    expect(isSuccess(result)).toEqual(true)
+})
+
+const defaultRules = fs.readFileSync(path.resolve(dirname, '..', 'rules', 'default.n3'))
+const testNames = ['default01', 'default02']
+for (const testName of testNames) {
+    test(testName, async () => {
+        const test = support(`${testName}.n3`)
+        const result = await applyRules(test, defaultRules)
+        expect(isSuccess(result)).toEqual(true)
+    })
+}
