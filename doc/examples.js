15cd336f4ad624ba54bd7593244db4a88ac7d9c5
Giacomo Citi <giacomo.citi@zazuko.com>
2025-03-18 22:03:17 +0100

    schema visualization

--- doc/examples.js
+++ doc/examples.js
@@ -1,5 +1,6 @@
 import data1 from './data2.ttl?raw'
 import schema from '../examples/schema.n3?raw'
+import vocabulary from '../examples/vocabulary.n3?raw'
 import vocabulary1 from '../packages/rdf2dot/vocabulary.ttl?raw'
 import barnard59 from '../examples/barnard59/barnard59.n3?raw'
 import pipeline1 from '../examples/barnard59/pipeline1.ttl?raw'
@@ -16,6 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('basic1').data = data1
     document.getElementById('default1').data = data1
 
-    showGraph(vocabulary1, schema, 'vocabulary1')
+    showGraph(data1, schema, 'schema1')
+    showGraph(vocabulary1, vocabulary, 'vocabulary1')
     showGraph(pipeline1, barnard59, 'pipeline1')
 })
